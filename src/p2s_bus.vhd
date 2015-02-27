39222ffc7d5761873b75e9d75822d9bec4dcd36c
Matthew McAllister <matthew.mcallister@berkeley.edu>
2015-02-26 22:31:32 -0800

    Fixed p2s_bus bug

--- src/p2s_bus.vhd
+++ src/p2s_bus.vhd
@@ -29,17 +29,10 @@ architecture behavior of p2s_bus is
 		-- falling edge of sclk, the highest bit is written and the register
 		-- is shifter left one bit. sclk runs at half clock speed to meet
 		-- setup and hold time requirements.
-		ST_WRITE,
-		-- Set finish flag for one cycle
-		ST_FINISH
+		ST_WRITE
 	);
 
 	signal state: state_type := ST_RESET;
-
-	-- Counter for number of bits written, tells when to go to finish state.
-	-- A variable would be nice, but state and signal control are separate this
-	-- way. I doubt this gets synthesized into sort of counter.
-	signal bits_written: natural := 0;
 
 	-- Data shift register
 	-- DST_LEN is DATA_WIDTH - 1 because the first address bit is
@@ -47,89 +40,56 @@ architecture behavior of p2s_bus is
 	constant DST_LEN: natural := DATA_WIDTH - 1;
 	signal dst: std_logic_vector(DST_LEN - 1 downto 0) := (others => '0');
 begin
-	state_control:
-	process (clock, reset)
-	begin
-		if reset = '1' then
-			state <= ST_RESET;
-		elsif rising_edge(clock) then
-			case state is
-			when ST_RESET => 
-				state <= ST_WRITE;
-			when ST_WRITE =>
-				-- Finish state while last bit is written.
-				if bits_written = DATA_WIDTH then
-					state <= ST_FINISH;
-				end if;
-			when ST_FINISH =>
-				state <= ST_WRITE;
-			end case;
-		end if;
-	end process;
-	
-	flag_control:
-	process (state, reset)
-	begin
-		if reset = '1' then
-			finish_flag <= '0';
-		else
-			case state is
-			when ST_RESET =>
-				active_flag <= '0';
-				finish_flag <= '0';
-			when ST_WRITE =>
-				active_flag <= '1';
-				finish_flag <= '0';
-			when ST_FINISH =>
-				active_flag <= '1';
-				finish_flag <= '1';
-			end case;
-		end if;
-	end process;
-	
-	data_assignments:
+	-- Yes, I still intend to separate state and data control
+	state_and_data_control:
 	process (clock, state, reset)
 		-- Internally track the value of sclk
-		variable sclk_sync: std_logic := '0';
+		variable sclk_sync:    std_logic := '0';
+		variable bits_written: natural range 0 to DATA_WIDTH - 1 := 0;
 	begin
 		if reset = '1' then
+			state        <= ST_RESET;
 			sclk_sync    := '0';
 			sclk         <= '0';
-			bits_written <= 0;
+			active_flag  <= '0';
+			finish_flag  <= '0';
+			bits_written := 0;
 			sdo          <= pdi(DATA_WIDTH - 1);
 			dst          <= pdi(DATA_WIDTH - 2 downto 0);
 		elsif rising_edge(clock) then
 			case state is 
 			when ST_RESET =>
+				state        <= ST_WRiTE;
 				sclk_sync    := '0';
 				sclk         <= '0';
-				bits_written <= 0;
+				active_flag  <= '0';
+				finish_flag  <= '0';
+				bits_written := 0;
 				sdo          <= pdi(DATA_WIDTH - 1);
 				dst          <= pdi(DATA_WIDTH - 2 downto 0);
 			when ST_WRITE => 
 				if sclk_sync = '0' then
 					sclk_sync    := '1';
 					sclk         <= '1';
-					bits_written <= bits_written + 1;
+					finish_flag <= '0';
 				else
+					if bits_written = DATA_WIDTH - 1 then
+						bits_written := 0;
+						-- Update data register
+						sdo <= pdi(DATA_WIDTH - 1);
+						dst <= pdi(DATA_WIDTH - 2 downto 0);
+						finish_flag <= '1';
+					else
+						bits_written := bits_written + 1;
+						-- Write out highest bit
+						sdo       <= dst(DST_LEN - 1);
+						dst       <= dst(DST_LEN - 2 downto 0) & '0';
+						finish_flag <= '0';
+					end if;
 					sclk_sync := '0';
 					sclk      <= '0';
-					-- Write out highest bit
-					sdo       <= dst(DST_LEN - 1);
-					dst       <= dst(DST_LEN - 2 downto 0) & '0';
+					active_flag <= '1';
 				end if;
-			when ST_FINISH => 
-				if sclk_sync = '1' then
-					sclk_sync := '0';
-					sclk      <= '0';
-				else
-					sclk_sync := '1';
-					sclk      <= '1';
-				end if;
-				bits_written <= 0;
-				-- Update data register
-				sdo <= pdi(DATA_WIDTH - 1);
-				dst <= pdi(DATA_WIDTH - 2 downto 0);
 			end case;
 		end if;
 	end process;
